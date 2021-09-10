381710fd003d869f0d25d36c2c2c70e04a3d4318
ThePrimeJade <primejadevi@pm.me>
2021-09-11 01:11:13 +0430

    change surf zoom level to 1.1 from 1.2

--- surf/config.h
+++ surf/config.h
@@ -54,7 +54,7 @@ static Parameter defconfig[ParameterLast] = {
 	[StrictTLS]           =       { { .i = 1 },     },
 	[Style]               =       { { .i = 1 },     },
 	[WebGL]               =       { { .i = 0 },     },
-	[ZoomLevel]           =       { { .f = 1.2 },   },
+	[ZoomLevel]           =       { { .f = 1.1 },   },
 };
 
 static UriParameters uriparams[] = {
