ac9e467c126e6fb58e64ffc2d96b9ae263fd760c
ThePrimeJade <primejadevi@pm.me>
2021-09-12 17:01:03 +0430

    remove dmenu following dwm colorscheme

--- dwm/config.h
+++ dwm/config.h
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-p", "run:", "-hp", "chromium", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-p", "run:", "-hp", "chromium", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 // /* custom executable commands */
 static const char *flotercmd[]  = { "st", "-t", "Terminal", NULL};
