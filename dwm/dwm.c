ab51846547009df7cb9bbfa7b14f367f86bdb625
ThePrimeJade <primejadevi@pm.me>
2021-09-13 23:13:34 +0430

    dwm: fix centered window title patch

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -767,8 +767,17 @@ drawbar(Monitor *m)
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
+    /* centered window title */
+        /* fix overflow when window name is bigger than window width */
+			int mid = (m->ww - (int)TEXTW(m->sel->name)) / 2 - x;
+        /* make sure name will not overlap on tags even when it is very long */
+			mid = mid >= lrpad / 2 ? mid : lrpad / 2;
+    /* centered window title */
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+//			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+    /* centered window title */
++			drw_text(drw, x, 0, w, bh, mid, m->sel->name, 0);
+    /* centered window title */
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
